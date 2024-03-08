cef39426209d0f0349f6d5031541cbbd1ac0d9ff
Isabel Louise Vicente <isabellouiseph@gmail.com>
2024-03-08 20:46:26 +0800

    added day 9

--- Day9/dom.js
+++ Day9/dom.js
@@ -8,16 +8,18 @@ console.log(heading);
 const heading2 = document.getElementsByTagName("h2");
 console.log(heading2[0]);
 
-//by querrySelector
+//by querySelector
 const listItems = document.querySelectorAll("#list li");
 console.log(listItems);
 
 //Modifying Elements
 //innerHTML
-heading[0].innerHTML = "Learning DOM Manipulation";
+heading[0].innerHTML = "<h6>Learning DOM Manipulation</h6>";
+// allows you to set the HTML content inside an element as a string. This means it includes all HTML tags and their content.
 
 //textContent
 heading2[0].textContent = "Element Manipulation";
+// returns or allows you to set the text content of an element, excluding any HTML tags. It returns only the text content inside the element, without any formatting or HTML structure.
 
 //setting attributes
 heading[0].setAttribute("class", "highlight");
@@ -32,6 +34,11 @@ const newParagraph = document.createElement("p");
 newParagraph.textContent = "This is a new paragraph.";
 
 container[0].appendChild(newParagraph);
+
+// Removing an element:
+const elementToRemove = document.querySelector("li");
+elementToRemove.remove();
+console.log("Removed Element:", elementToRemove);
 
 //handling events
 button.addEventListener("click", addListItem);
